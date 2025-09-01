42224165d66eee124ca81cfbad76778c3d3c1853
Anja Sheppard <amstrudy@gmail.com>
2025-09-01 12:23:15 -0400

    fix crater generation issue when resolution changes

--- src/lunasynth/terrain_enhancement.py
+++ src/lunasynth/terrain_enhancement.py
@@ -1053,10 +1053,11 @@ class Terrain:
         if terrain_dict.get("add_craters", True):
             crater_field = CraterField()
             crater_density = sample(terrain_dict["crater_field"]["density"])
+            height, width = elevation_data.shape
             crater_field.generate(
                 k=crater_density,
-                size_y=ymax - ymin,
-                size_x=xmax - xmin,
+                size_y=height,
+                size_x=width,
                 h_min=terrain_dict["crater_field"]["h_min"],
                 h_max=terrain_dict["crater_field"]["h_max"],
                 x_init=0,
